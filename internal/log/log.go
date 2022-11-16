006860b825c0fd0a0228685393d970da0b68f7e8
Y7n05h <Y7n05h@protonmail.com>
2022-11-16 20:08:38 +0800

    Update

--- internal/log/log.go
+++ internal/log/log.go
@@ -21,30 +21,27 @@ var (
 )
 
 func InitLog(enableLog bool, useTerminal bool) {
-	var lg *zap.Logger
 	if enableLog {
-		lg = zap.NewNop()
-	} else {
+		Log = zap.NewNop().Sugar()
+		return
+	}
+	encoderCfg := getLogEncoder()
+	if !useTerminal {
 		file, _ := os.OpenFile("/tmp/1.log", os.O_APPEND|os.O_CREATE, 0644)
 		writeSyncer := zapcore.AddSync(file)
-		encoderCfg := getLogEncoder()
-		if useTerminal {
-
-			encoder := zapcore.NewJSONEncoder(encoderCfg)
-			core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
-			lg = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
-		} else {
-			loggerCfg := zap.NewDevelopmentConfig()
-			loggerCfg.EncoderConfig = encoderCfg
-			var err error
-			lg, err = loggerCfg.Build(zap.AddCaller(), zap.AddStacktrace(zap.FatalLevel))
-			if err != nil {
-				os.Exit(-1)
-			}
-		}
-
+		encoder := zapcore.NewJSONEncoder(encoderCfg)
+		core := zapcore.NewCore(encoder, writeSyncer, zapcore.DebugLevel)
+		Log = zap.New(core, zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel)).Sugar()
+		return
+	}
+	loggerCfg := zap.NewDevelopmentConfig()
+	loggerCfg.EncoderConfig = encoderCfg
+	lg, err := loggerCfg.Build(zap.AddCaller(), zap.AddStacktrace(zap.FatalLevel))
+	if err != nil {
+		os.Exit(-1)
 	}
 	Log = lg.Sugar()
+
 }
 func init() {
 	InitLog(constexpr.EnableLog, constexpr.LogOutputToTerminal)
