c7b1463567eb8dd7e5931adf380eff12c144e3fd
Y7n05h <Y7n05h@protonmail.com>
2022-12-25 16:19:54 +0800

    refactor: clean code

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -14,14 +14,12 @@ func FullVersion() string {
 }
 
 const (
-	EnableLog           = true
-	LogOutputToTerminal = false
+	EnableLog = true
 )
 const (
 	BridgeName = "blitz0"
 	Mtu        = 1500
 	VxlanId    = 666
 	VXLANPort  = 12564
-	VXLANGroup = "239.1.1.1"
 	VXLANName  = "blitznet"
 )
