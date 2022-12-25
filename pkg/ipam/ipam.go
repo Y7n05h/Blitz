553e4fe7ae088297fbb634de6e0028808b8c3816
Y7n05h <Y7n05h@protonmail.com>
2022-12-25 20:31:12 +0800

    refactor: 缓存最大可用 IP 数，防止反复计算 refactor: 根据剩余IP数量采取不同的分配策略，,提高 IPAM 分配效率

--- pkg/ipam/ipam.go
+++ pkg/ipam/ipam.go
@@ -25,22 +25,38 @@ func init() {
 type Ipam struct {
 	//Subnet is available in this node
 	Subnet *ipnet.IPNet
-	//Gateway is the gateway of devices in this node
-	//Sometimes, Gateway may not equal to Subnet
-	Gateway *ipnet.IPNet
+	//gateway is the gateway of devices in this node
+	//Sometimes, gateway may not equal to Subnet
+	gateway      *ipnet.IPNet
+	validIPCount uint64
 	//	IP  -> ID
 	AllocRecord *bimap.BiMap[string, string]
 }
 
+func validIpCount(subnet *ipnet.IPNet) uint64 {
+	ones, bits := subnet.Mask.Size()
+	size := bits - ones
+	if size < 2 {
+		log.Log.Warn("too small Subnet")
+		return 0
+	}
+	if size > 64 {
+		log.Log.Warn("too big Subnet")
+		size = 64
+	}
+	// 减去 2 个不可用于 Pod 的地址（主机号全为 0 或全为 1）
+	//和 1 个 Blitz 的保留地址（主机号为 1）
+	result := (uint64(1) << size) - 3
+	log.Log.Debugf("Max Size %d", result)
+	return result
+}
 func (r Ipam) MarshalJSON() ([]byte, error) {
 	log.Log.Debug("Marshal Ipam Begin")
 	data, err := json.Marshal(&struct {
 		Subnet      ipnet.IPNet
-		Gateway     ipnet.IPNet
 		AllocRecord map[string]string
 	}{
 		Subnet:      *r.Subnet,
-		Gateway:     *r.Gateway,
 		AllocRecord: r.AllocRecord.GetForwardMap(),
 	})
 	if err != nil {
@@ -53,25 +69,30 @@ func (r *Ipam) UnmarshalJSON(data []byte) error {
 	log.Log.Debug("Unmarshal Ipam Begin")
 	record := &struct {
 		Subnet      ipnet.IPNet
-		Gateway     ipnet.IPNet
 		AllocRecord map[string]string
 	}{}
 	if err := json.Unmarshal(data, record); err != nil {
 		return err
 	}
 	r.Subnet = &record.Subnet
-	r.Gateway = &record.Gateway
+	r.gateway = ipnet.FromIPAndMask(cip.NextIP(record.Subnet.IP), record.Subnet.Mask)
 	log.Log.Debugf("Get PodCIDR: %s", r.Subnet.String())
 	if record.AllocRecord != nil {
 		r.AllocRecord = bimap.NewBiMapFromMap[string, string](record.AllocRecord)
 	} else {
 		r.AllocRecord = bimap.NewBiMap[string, string]()
 	}
+	r.validIPCount = validIpCount(r.Subnet)
 	log.Log.Debug("Unmarshal Ipam Finished")
 	return nil
 }
 func New(subnet *ipnet.IPNet) *Ipam {
-	return &Ipam{Subnet: subnet, Gateway: ipnet.FromIPAndMask(cip.NextIP(subnet.IP), subnet.Mask), AllocRecord: bimap.NewBiMap[string, string]()}
+	return &Ipam{
+		Subnet:       subnet,
+		gateway:      ipnet.FromIPAndMask(cip.NextIP(subnet.IP), subnet.Mask),
+		validIPCount: validIpCount(subnet),
+		AllocRecord:  bimap.NewBiMap[string, string](),
+	}
 }
 func (r *Ipam) Alloced(ip *net.IP) bool {
 	log.Log.Debugf("Ipam %#v", r.AllocRecord)
@@ -87,7 +108,7 @@ func (r *Ipam) getAvailableLen() int {
 	return bits - ones
 }
 func (r *Ipam) GetGateway() *ipnet.IPNet {
-	return r.Gateway
+	return r.gateway
 }
 func ipToInt(ip net.IP) *big.Int {
 	if v := ip.To4(); v != nil {
@@ -99,6 +120,11 @@ func ipToInt(ip net.IP) *big.Int {
 func intToIP(i *big.Int) net.IP {
 	return i.Bytes()
 }
+func (r *Ipam) saveRecord(ip net.IP, id string) *ipnet.IPNet {
+	log.Log.Debugf("Alloc IP %s", ip.String())
+	r.AllocRecord.Insert(ip.String(), id)
+	return &ipnet.IPNet{IP: ip, Mask: r.Mask()}
+}
 func (r *Ipam) Alloc(id string) (*ipnet.IPNet, error) {
 	log.Log.Debugf("Ipam %#v", r.AllocRecord)
 	subnet, ok := r.GetIPByID(id)
@@ -106,35 +132,34 @@ func (r *Ipam) Alloc(id string) (*ipnet.IPNet, error) {
 		return subnet, nil
 	}
 
-	log.Log.Debugf("Alloc Subnet %s", r.Subnet.String())
-	size := r.getAvailableLen()
-	if size < 2 {
-		return nil, fmt.Errorf("too small Subnet")
-	}
-	if size > 64 {
-		log.Log.Warn("too big Subnet")
-		size = 64
-	}
-	// 减去 2 个不可用于 Pod 的地址（主机号全为 0 或全为 1）
-	//和 1 个 Blitz 的保留地址（主机号为 1）
-	max := (uint64(1) << size) - 3
-
-	log.Log.Debugf("Max Size %d", max)
-	if uint64(r.AllocRecord.Size()) >= max {
+	max := r.validIPCount
+	usedIPAddr := uint64(r.AllocRecord.Size())
+	if usedIPAddr >= max {
 		return nil, fmt.Errorf("subnet have no available ip addr")
 	}
 	idx := big.NewInt(0)
-	for {
-		idx.SetUint64((rand.Uint64() % max) + 2)
+	if (usedIPAddr << 1) <= max {
+		//剩余的IP地址充足，通过遍历来寻找可用的 IP 地址
+		for {
+			idx.SetUint64((rand.Uint64() % max) + 2)
+			log.Log.Debugf("Rand Number:%v", idx)
+			ipNum := ipToInt(r.Subnet.IP)
+			ip := intToIP(ipNum.Add(ipNum, idx))
+			if !r.Alloced(&ip) {
+				return r.saveRecord(ip, id), nil
+			}
+		}
+	}
+	for n := uint64(0); n < max; n++ {
+		idx.SetUint64(n + 2)
 		log.Log.Debugf("Rand Number:%v", idx)
 		ipNum := ipToInt(r.Subnet.IP)
 		ip := intToIP(ipNum.Add(ipNum, idx))
 		if !r.Alloced(&ip) {
-			r.AllocRecord.Insert(ip.String(), id)
-			log.Log.Debugf("Alloc IP %s", ip.String())
-			return &ipnet.IPNet{IP: ip, Mask: r.Mask()}, nil
+			return r.saveRecord(ip, id), nil
 		}
 	}
+	return nil, fmt.Errorf("alloc IP Failed")
 }
 func (r *Ipam) Release(id string) error {
 	log.Log.Debug("Release id")
@@ -160,5 +185,5 @@ func (r *Ipam) GetIPByID(id string) (*ipnet.IPNet, bool) {
 	return &ipnet.IPNet{IP: ip, Mask: r.Mask()}, true
 }
 func (r *Ipam) Mask() net.IPMask {
-	return r.Gateway.Mask
+	return r.Subnet.Mask
 }
